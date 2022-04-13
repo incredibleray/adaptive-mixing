86c6a65c2c5e2b6c195c645e4cb570eff948c279
Ray Xu <ruijxu@microsoft.com>
2022-04-13 10:22:40 -0700

    c

--- mixing.py
+++ mixing.py
@@ -20,23 +20,27 @@ rleOutSeqLen=0
 
 lzwWeight=500
 
-for i in range(20, len(inputSeq), 20):
+for i in range(500, len(inputSeq), 100):
   seq=inputSeq[:i]
 
-  print("input sequence\n{}".format(seq))
+  # print("input sequence\n{}".format(seq))
+  print("input sequence len={}".format(i))
   # lzwSeq=lz.compress(seq)
   # rleSeq=rle2.encode_message(seq)
-  lzwSeq, _, _=lz77.encode_lz77(seq, 8, 3)
-  rleSeq, _, _=lz77.encode_lz77(seq, 49, 7)
-  print("lzw_8_3 encoded sequence\n{}".format(lzwSeq))
+  lzwSeq, _, _=lz77.encode_lz77(seq, 128, 12)
+  rleSeq, _, _=lz77.encode_lz77(seq, 250, 24)
+  print("lzw_128_7 encoded sequence len={}".format(len(lzwSeq)))
+  print("lzw_250_8 encoded sequence len={}".format(len(rleSeq)))
 
   lzwProbTable=dict(Counter(lzwSeq))
   rleProbTable=dict(Counter(rleSeq))
   
-  print("lzw_8_3 probability table")
+  print("lzw_128_7 probability table")
   probabilityTable.printTable(lzwProbTable)
-  print("lzw_49_7 probability table")
+  probabilityTable.printentropy(lzwProbTable)
+  print("lzw_250_8 probability table")
   probabilityTable.printTable(rleProbTable)
+  probabilityTable.printentropy(rleProbTable)
 
   weightedProb={}
   for p in lzwProbTable:
@@ -46,8 +50,9 @@ for i in range(20, len(inputSeq), 20):
     weightedProb[p]=(weightedProb.get(p) or 0) +rleProbTable[p]*(1000-lzwWeight)
   
   # mixedProbTable=M.mixProbTable(lzwProbTable, rleProbTable, lzwWeight)
-  print("weighted probability table")
+  print("mixed probability table")
   probabilityTable.printTable(weightedProb)
+  probabilityTable.printentropy(weightedProb)
 
 # Encode the message
   lzwOutSeq=None
@@ -56,12 +61,12 @@ for i in range(20, len(inputSeq), 20):
   lzwOutSeqLen=ae.encodedLen(lzwSeq, weightedProb)
   rleOutSeqLen=ae.encodedLen(rleSeq, weightedProb)
 
-  print("lzw_8_3 mixed probability encoded length={}".format(lzwOutSeqLen))
-  print("lzw_49_7 mixed probability encoded length={}".format(rleOutSeqLen))
+  print("lzw_128_7 mixed probability encoded length={}".format(lzwOutSeqLen))
+  print("lzw_250_8 mixed probability encoded length={}".format(rleOutSeqLen))
 
   lzwWeight=int(1000*rleOutSeqLen/(lzwOutSeqLen+rleOutSeqLen))
 
-  print("lzw_8_3 weight={:1.3f}, lzw_49_7 weight={:1.3f}".format(lzwWeight/1000, 1-lzwWeight/1000))
+  print("lzw_128_7 weight={:1.3f}, lzw_250_8 weight={:1.3f}".format(lzwWeight/1000, 1-lzwWeight/1000))
 
   # if len(lzwOutSeq) < len(rleOutSeq):
   #   outSeq=lzwOutSeq
--- probabilityTable.py
+++ probabilityTable.py
@@ -1,3 +1,5 @@
+import math
+
 def printTable(table):
   total=sum(table.values())
   tableOut=""
@@ -5,3 +7,11 @@ def printTable(table):
     tableOut+="{}:{:1.3f},\t".format(k, v/total)
 
   print(tableOut)
+
+def printentropy(table):
+  total=sum(table.values())
+  entropy=0
+  for k, v in table.items():
+    entropy+=-math.log2(v/total)
+
+  print("entropy is {:.2f}".format(entropy))
